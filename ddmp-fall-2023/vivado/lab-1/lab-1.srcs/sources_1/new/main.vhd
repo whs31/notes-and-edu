0ae03a3143a5df503154471fb074795b1ab48df8
whs31 <75864292+whs31@users.noreply.github.com>
2023-11-15 23:41:03 +0300

    lab 2

--- ddmp-fall-2023/vivado/lab-1/lab-1.srcs/sources_1/new/main.vhd
+++ ddmp-fall-2023/vivado/lab-1/lab-1.srcs/sources_1/new/main.vhd
@@ -36,7 +36,7 @@ entity lab_1 is
     );
 end lab_1;
 
-architecture rtl of lab_1 is
+architecture impl of lab_1 is
     signal and_gate: std_logic;
     signal or_gate: std_logic;
     signal xor_gate: std_logic;
@@ -45,7 +45,7 @@ architecture rtl of lab_1 is
     signal nor_gate: std_logic;
     signal xnor_gate: std_logic;
 begin
-    and_gate <= a and b;
+    and_gate <= a and b;  
     or_gate <= a or b;
     xor_gate <= a xor b;
     not_gate <= not a;
@@ -60,4 +60,52 @@ begin
     out_nand <= nand_gate;
     out_nor <= nor_gate;
     out_xnor <= xnor_gate;
-end rtl;
+end impl;
+
+-- -- lab 2 part 1
+-- library ieee; 
+-- use ieee.std_logic_1164.all; 
+
+-- entity lab_2_1 is 
+--     port (
+--         din: in std_logic_vector(2 downto 0);
+--         dout: out std_logic_vector(7 downto 0)
+--     );
+-- end lab2_1
+
+-- architecture impl of lab_2_1 is 
+-- begin
+--     with din select dout <=
+--         "00000001" when "000",
+--         "00000010" when "001",
+--         "00000100" when "010",
+--         "00001000" when "011",
+--         "00010000" when "100",
+--         "00100000" when "101",
+--         "01000000" when "110",
+--         "10000000" when others;
+-- end impl;
+
+-- -- lab 2 part 2 
+-- library IEEE;
+-- use IEEE.STD_LOGIC_1164.ALL;
+
+-- entity lab_2_2 is 
+--     port (
+--         din: in std_logic_vector(7 downto 0);
+--         dout: out std_logic_vector(3 downto 0)
+--     );
+-- end lab_2_2;
+
+-- architecture impl of lab_2_2 is 
+-- begin
+--     with din select dout <= 
+--         "0001" when "00000010", 
+--         "0010" when "00000100", 
+--         "0011" when "00001000", 
+--         "0100" when "00010000", 
+--         "0101" when "00100000", 
+--         "0110" when "01000000", 
+--         "0111" when "10000000", 
+--         "1000" when others;
+-- end impl;
